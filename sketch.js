2a53b8c86db351851b0ad0a75d3dc78a561ad3ec
Paurvvik <82900881+Paurvvik@users.noreply.github.com>
2021-04-24 20:44:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var platform1,platform2
+var countDistanceX=0
+var gape=60;
+function preload()
+{}
+
+function setup() {
+  createCanvas(9000, 668);
+ 
+  for(var i=0;i<6;i++){
+    platform1=new Platform(countDistanceX);
+    countDistanceX=countDistanceX+platform1.rw+gape;
+    gape=random([0,0,0,0,60]);
+  }
+  
+  
+}
+
+function draw() {
+  background('skyblue'); 
+ drawSprites();
+}
+
